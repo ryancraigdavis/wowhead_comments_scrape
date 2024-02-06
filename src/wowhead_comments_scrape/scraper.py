2faeb31141a82ec2874f4b9073ef9aaadb8ac209
Ryan Davis <ryancraigdavis@github.com>
2024-02-05 23:34:48 -0800

    1 Code needs to be switched from BS4 to Scrapy

--- src/wowhead_comments_scrape/scraper.py
+++ src/wowhead_comments_scrape/scraper.py
@@ -4,26 +4,30 @@ from bs4 import BeautifulSoup
 def scrape_wowhead(url):
     # Send a GET request to the URL
     response = requests.get(url)
+    print(response.status_code)
     
+    # This code won't work, switch to Scrapy
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
         # Parse the HTML content of the page using Beautiful Soup
         soup = BeautifulSoup(response.text, 'html.parser')
         
-        # Extract the desired information from the parsed HTML
-        # Adjust this part based on the specific content you want to scrape
+        # Extract the comments based on the class "Comment wrapper"
+        comments = soup.find_all(class_='comment-wrapper')
         
-        # Example: Print the titles of all elements with the class 'title'
-        titles = soup.find_all(class_='title')
-        for title in titles:
-            print(title.text)
-            
+        # Print the content of each comment
+        for comment in comments:
+            # Modify this part based on the specific structure of the comments
+            comment_content = comment.find(class_='comment-text').text
+            print(comment_content)
+
+
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 
 if __name__ == "__main__":
     # Replace the URL with the specific webpage you want to scrape
-    url = "https://www.wowhead.com/wotlk"
+    url = "https://www.wowhead.com/wotlk/quest=12199"
     
     # Call the function with the URL
     scrape_wowhead(url)
